951680c2512a5a0fba6c34664709e7d10a5a0b7d
yousung1020 <choiyousung1020@gmail.com>
2026-01-05 23:21:04 +0900

    chore: 프로젝트 초기화 (Spring Boot 4.0.1, jdk 21)

---
+++ src/test/java/com/example/pace/PaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
