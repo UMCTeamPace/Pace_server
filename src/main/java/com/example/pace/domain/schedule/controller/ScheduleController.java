2627af5276227c411f35eff69dc80ee0da716c87
bini0918 <165636716+bini0918@users.noreply.github.com>
2026-01-19 20:20:17 +0900

    feat: ScheduleController 스웨거 어노테이션 설정

--- src/main/java/com/example/pace/domain/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/pace/domain/schedule/controller/ScheduleController.java
@@ -2,6 +2,8 @@ package com.example.pace.domain.schedule.controller;
 
 import com.example.pace.domain.schedule.dto.request.ScheduleRequestDto;
 import com.example.pace.domain.schedule.service.ScheduleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/schedules")
+@Tag(name = "Schedule")
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
     // 일정 생성 API
     @PostMapping
+    @Operation(summary = "일정 생성", description = "새로운 일정을 생성하고 저장합니다.")
     public ResponseEntity<Long> createSchedule(
             @RequestParam Long memberId, //임시
             @RequestBody ScheduleRequestDto requestDto
