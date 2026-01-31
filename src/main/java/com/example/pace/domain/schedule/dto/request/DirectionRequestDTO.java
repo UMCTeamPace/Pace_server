815046f74c330b16bf2c0ac3637b192d734a874f
dmlwjds2 <dmlwjd0809@hanyang.ac.kr>
2026-02-01 00:54:49 +0900

    refactor: pr review 반명/non-block 부분 제외

--- src/main/java/com/example/pace/domain/schedule/dto/request/DirectionRequestDTO.java
+++ src/main/java/com/example/pace/domain/schedule/dto/request/DirectionRequestDTO.java
@@ -1,4 +1,20 @@
 package com.example.pace.domain.schedule.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class DirectionRequestDTO {
+
+    private String origin;
+    private String destination;
+    private Long departureTime;
+    private Long arrivalTime;
+    private String transitMode;
+    private String routingPreference;
 }
--- src/main/java/com/example/pace/domain/schedule/infrastructure/GoogleDirectionApiClient.java
+++ src/main/java/com/example/pace/domain/schedule/infrastructure/GoogleDirectionApiClient.java
@@ -1,8 +1,12 @@
 package com.example.pace.domain.schedule.infrastructure;
 
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.queryParam;
+
+import com.example.pace.domain.schedule.dto.request.DirectionRequestDTO;
 import com.example.pace.domain.schedule.infrastructure.dto.GoogleDirectionApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -11,29 +15,33 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class GoogleDirectionApiClient {
 
     private final WebClient googleDirectionsWebClient;
+    private static final String TRANSIT_MODE = "transit";
 
-    public GoogleDirectionApiResponse getDirections(
-            String origin,
-            String destination,
-            Long departureTime,
-            Long arrivalTime,
-            String transitMode,
-            String routingPreference
-    ) {
+    public GoogleDirectionApiResponse getDirections(DirectionRequestDTO request) {
+
+        String origin = request.getOrigin();
+        String destination = request.getDestination();
+        Long departureTime = request.getDepartureTime();
+        Long arrivalTime = request.getArrivalTime();
+        String transitMode = request.getTransitMode();
+        String routingPreference = request.getRoutingPreference();
+
         return googleDirectionsWebClient.get()
                 .uri(uriBuilder -> {
                     uriBuilder
                             .path("/maps/api/directions/json")
                             .queryParam("origin", origin)
                             .queryParam("destination", destination)
-                            .queryParam("mode", "transit"); // 대중교통 모드 고정
+                            .queryParam("mode", TRANSIT_MODE);//final 변수 사용해서 대중교통 모드 지정
 
+                    //도착시간or출발시간 분기처리
                     if (arrivalTime != null) {
                         uriBuilder.queryParam("arrival_time", arrivalTime);
-                    }
-
-                    if (departureTime != null) {
+                    } else if (departureTime != null) {
+                        // 도착 시간이 없을 때만 출발 시간을 설정
                         uriBuilder.queryParam("departure_time", departureTime);
+                    } else {
+                        //'departure_time=now' 기본값(생략 가능)
                     }
 
                     if (transitMode != null) {
