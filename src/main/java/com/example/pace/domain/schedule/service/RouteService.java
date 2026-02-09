4b0f9349241b7ef99088a5ff320a1551c667b646
dmlwjds2 <dmlwjd0809@hanyang.ac.kr>
2026-02-10 01:16:14 +0900

    feat: 도보상세 제거 및 sequence값 재설정

--- src/main/java/com/example/pace/domain/schedule/service/RouteService.java
+++ src/main/java/com/example/pace/domain/schedule/service/RouteService.java
@@ -1,7 +1,6 @@
 package com.example.pace.domain.schedule.service;
 
 import static com.example.pace.domain.schedule.enums.SearchWay.MIN_TIME;
-import static com.example.pace.domain.schedule.enums.SearchWay.MIN_TRANSFER;
 
 import com.example.pace.domain.schedule.converter.RouteResDTOConverter;
 import com.example.pace.domain.schedule.dto.request.DirectionRequestDTO;
@@ -10,7 +9,6 @@ import com.example.pace.domain.schedule.dto.response.RouteApiResDto;
 import com.example.pace.domain.schedule.dto.response.RouteListResDTO;
 import com.example.pace.domain.schedule.dto.response.info.RouteDetailInfoResDTO;
 import com.example.pace.domain.schedule.dto.response.info.TransitRouteDetailInfoResDTO;
-import com.example.pace.domain.schedule.enums.SearchWay;
 import com.example.pace.domain.schedule.enums.TransitType;
 import com.example.pace.domain.schedule.infrastructure.GoogleDirectionApiClient;
 import com.example.pace.domain.schedule.infrastructure.dto.GoogleDirectionApiResponse;
@@ -18,7 +16,6 @@ import com.example.pace.domain.transit.dto.SubwayStationDTO;
 import com.example.pace.domain.transit.entity.BusInfo;
 import com.example.pace.domain.transit.service.BusNetworkService;
 import com.example.pace.domain.transit.service.SubwayNetworkService;
-import java.time.ZoneId;
 import java.util.Comparator;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -139,7 +136,11 @@ public class RouteService {
 
                     transit.setStationPath(stationPath);
                 }
+
+
             }
+
+
         }
     }
 
