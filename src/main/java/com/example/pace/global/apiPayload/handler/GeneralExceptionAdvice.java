0e666bcf434e9baf27df1c4018fc90c05e520b6a
yousung1020 <choiyousung1020@gmail.com>
2026-03-02 13:29:30 +0900

    feat & refactor: Nginx 리버스 프록시 도입 및 보안 강화

--- src/main/java/com/example/pace/global/apiPayload/handler/GeneralExceptionAdvice.java
+++ src/main/java/com/example/pace/global/apiPayload/handler/GeneralExceptionAdvice.java
@@ -4,15 +4,19 @@ import com.example.pace.global.apiPayload.ApiResponse;
 import com.example.pace.global.apiPayload.code.BaseErrorCode;
 import com.example.pace.global.apiPayload.code.GeneralErrorCode;
 import com.example.pace.global.apiPayload.exception.GeneralException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GeneralExceptionAdvice {
     // 커스텀 예외 처리
     @ExceptionHandler(GeneralException.class)
     public ResponseEntity<ApiResponse<Void>> handleException(GeneralException e) {
+        log.error("Internal server error:", e);
+
         return ResponseEntity
                 .status(e.getCode().getHttpStatus())
                 .body(ApiResponse.onFailure(
@@ -24,12 +28,13 @@ public class GeneralExceptionAdvice {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<String>> handleException(Exception e) {
         BaseErrorCode code = GeneralErrorCode.INTERNAL_SERVER_ERROR;
+        log.error("Internal server error:", e);
 
         return ResponseEntity
                 .status(code.getHttpStatus())
                 .body(ApiResponse.onFailure(
                         code,
-                        e.getMessage()
+                        "서버 내부 오류가 발생하였습니다."
                 ));
     }
 }
