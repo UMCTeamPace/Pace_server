727664ed282885048815ec9f8468cc5df729f9fa
yousung1020 <choiyousung1020@gmail.com>
2026-04-08 20:15:38 +0900

    fix: 병합 충돌 해결

--- src/main/java/com/example/pace/domain/member/service/SettingService.java
+++
@@ -1,9 +0,0 @@
-package com.example.pace.domain.member.service;
-
-import com.example.pace.domain.member.dto.request.SettingUpdateRequestDTO;
-import com.example.pace.domain.member.dto.response.SettingResponseDTO;
-
-public interface SettingService {
-    SettingResponseDTO getMySetting(Long memberId);
-    SettingResponseDTO updateMySetting(Long memberId, SettingUpdateRequestDTO request);
-}
--- src/main/java/com/example/pace/domain/schedule/service/ScheduleCommandService.java
+++
@@ -1,4 +0,0 @@
-package com.example.pace.domain.schedule.service;
-
-public class ScheduleCommandService {
-}
